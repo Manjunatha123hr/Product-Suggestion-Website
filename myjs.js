a98a0e55214c62a2553b41c41698b6c397cd1324
Manjunatha H R <110518064+Manjunatha123hr@users.noreply.github.com>
2022-11-24 07:09:00 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,5 @@
+const myheading=document.querySelector('h1');
+myheading.textContent='Hello World';
+document.querySelector('html').addEventListener('click', function () {
+    alert('Ouch! Stop poking me!');
+  });
